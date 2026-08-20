f3a99a5542e2d074a5d34c45cccf961f7ffeb831
JESINDA RACHEL R <jesindarachel@gmail.com>
2026-08-20 15:55:52 +0530

    To do string compression

---
+++ Compression.java
@@ -0,0 +1,24 @@
+public class Compression
+{
+    public static void main(String[] args)
+    {
+        String a = "aaabbccccddeff";
+        String b = "";
+        int c = 1;
+
+        for(int i = 0; i < a.length(); i++)
+        {
+            if(i + 1 < a.length() && a.charAt(i) == a.charAt(i + 1))
+            {
+                c++;
+            }
+            else
+            {
+                b = b + a.charAt(i) + c;
+                c = 1;
+            }
+        }
+
+        System.out.println(b);
+    }
+}
